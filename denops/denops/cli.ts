d8386620fb83ffd1879af85d6094a54c71d8559b
Alisue <lambdalisue@hashnote.net>
2021-02-13 13:31:07 +0900

    Add 'updateContext()' to update global context

--- denops/denops/cli.ts
+++ denops/denops/cli.ts
@@ -2,13 +2,13 @@ import { flags } from "./deps.ts";
 import { Service } from "./service.ts";
 import { createVim } from "./host/vim.ts";
 import { createNeovim } from "./host/nvim.ts";
-import { context } from "./context.ts";
+import { updateContext } from "./context.ts";
 
 async function main(): Promise<void> {
   const options = flags.parse(Deno.args);
-  Object.assign(context, {
-    mode: options.mode ?? context.mode,
-    debug: options.debug ?? context.debug,
+  const context = updateContext({
+    mode: options.mode,
+    debug: options.debug,
   });
   const createHost = context.mode === "vim" ? createVim : createNeovim;
   const host = createHost(Deno.stdin, Deno.stdout);
