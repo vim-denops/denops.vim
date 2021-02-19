31930952e57cf7bfc6ca71c8d9bb651745f1284d
Alisue <lambdalisue@hashnote.net>
2021-02-19 09:01:09 +0900

    Update msgpack_rpc

--- denops/denops/deps.ts
+++ denops/denops/deps.ts
@@ -1,4 +1,4 @@
 export * as flags from "https://deno.land/std@0.86.0/flags/mod.ts";
-export * as msgpackRpc from "https://deno.land/x/msgpack_rpc@v2.4/mod.ts";
+export * as msgpackRpc from "https://deno.land/x/msgpack_rpc@v2.6/mod.ts";
 export * as vimChannelCommand from "https://deno.land/x/vim_channel_command@v0.1/mod.ts";
 export * as denops from "https://deno.land/x/denops@v0.5/mod.ts";
