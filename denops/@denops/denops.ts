f06ba0297b67d5c5176b725f2a9f04e3d25305bb
Alisue <lambdalisue@hashnote.net>
2021-04-29 21:03:42 +0900

    Add doc comments on public interface

--- denops/@denops/denops.ts
+++ denops/@denops/denops.ts
@@ -6,8 +6,26 @@ import {
   WorkerWriter,
 } from "./deps.ts";
 
+/**
+ * Context which is expanded to the local namespace (l:)
+ */
 export type Context = Record<string, unknown>;
 
+/**
+ * Denpos is a facade instance visible from each denops plugins.
+ *
+ * Plugins use the denops instance to
+ *
+ * 1. Communicate with other plugins
+ * 2. Communicate with the host (Vim/Neovim)
+ * 3. Register them msgpack-rpc APIs
+ *
+ * The instance is thread-local singleton. Plugins need to refer
+ * the instance through `Denops.get()` static method.
+ *
+ * Note that plugins should NOT use the instance directly.
+ * Use it through `denops_std` module instead.
+ */
 export class Denops {
   private static instance?: Denops;
 
@@ -38,7 +56,9 @@ export class Denops {
   }
 
   /**
-   * Start main event-loop of the plugin
+   * Start denops mainloop for the plugin.
+   *
+   * @param main: An initialization function of the mainloop.
    */
   static start(init: (denops: Denops) => Promise<void> | void): void {
     const denops = Denops.get();
@@ -55,6 +75,13 @@ export class Denops {
     return this.#name;
   }
 
+  /**
+   * Dispatch an arbitrary function of an arbitrary plugin and return the result.
+   *
+   * @param name: A plugin registration name.
+   * @param fn: A function name in the API registration.
+   * @param args: Arguments of the function.
+   */
   async dispatch(
     name: string,
     fn: string,
@@ -63,20 +90,40 @@ export class Denops {
     return await this.#session.call("dispatch", name, fn, ...args);
   }
 
+  /**
+   * Call an arbitrary function of Vim/Neovim and return the result
+   *
+   * @param fn: A function name of Vim/Neovim.
+   * @param args: Arguments of the function.
+   */
   async call(fn: string, ...args: unknown[]): Promise<unknown> {
     return await this.#session.call("call", fn, ...args);
   }
 
+  /**
+   * Execute an arbitrary command of Vim/Neovim under a given context.
+   *
+   * @param cmd: A command expression to be executed.
+   * @param ctx: A context object which is expanded to the local namespace (l:)
+   */
   async cmd(cmd: string, ctx: Context = {}): Promise<void> {
     await this.#session.call("call", "denops#api#cmd", cmd, ctx);
   }
 
+  /**
+   * Evaluate an arbitrary expression of Vim/Neovim under a given context and return the result.
+   *
+   * @param expr: An expression to be evaluated.
+   * @param ctx: A context object which is expanded to the local namespace (l:)
+   */
   async eval(expr: string, ctx: Context = {}): Promise<unknown> {
     return await this.#session.call("call", "denops#api#eval", expr, ctx);
   }
 
   /**
    * Extend dispatcher of the internal msgpack_rpc session
+   *
+   * @param dispatcher: An object which key and value become API function name and callback respectively.
    */
   extendDispatcher(dispatcher: Dispatcher): void {
     this.#session.extendDispatcher(dispatcher);
