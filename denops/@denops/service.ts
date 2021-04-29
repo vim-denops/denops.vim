22f4913c39d7bf662f02b3e992c728a9867bf66b
Alisue <lambdalisue@hashnote.net>
2021-04-29 19:24:46 +0900

    Integrate codes in `plugin.ts` into `service.ts`

--- denops/@denops/service.ts
+++ denops/@denops/service.ts
@@ -1,9 +1,11 @@
-import { Session } from "./deps.ts";
-import { runPlugin } from "./plugin.ts";
+import { Api } from "./api.ts";
 import { Host } from "./host/mod.ts";
+import { Session, DispatcherFrom } from "./deps.ts";
+import { ensureArray, ensureRecord, ensureString } from "./utils.ts";
+import { path, WorkerReader, WorkerWriter } from "./deps.ts";
 
 export class Service {
-  #plugins: { [key: string]: Session };
+  #plugins: Record<string, Session>;
   #host: Host;
 
   constructor(host: Host) {
@@ -19,20 +21,36 @@ export class Service {
     if (this.#plugins[name]) {
       return;
     }
-    const session = runPlugin(this, {
-      name,
-      script,
-    });
+    const worker = new Worker(
+      new URL(path.toFileUrl(script).href, import.meta.url).href,
+      {
+        name: name,
+        type: "module",
+        deno: {
+          namespace: true,
+        },
+      },
+    );
+    const reader = new WorkerReader(worker);
+    const writer = new WorkerWriter(worker);
+    const session = new Session(reader, writer, buildDispatcher(this));
+    session
+      .listen()
+      .then()
+      .catch((e: Error) => {
+        console.error("Plugin server is closed with error:", e);
+      });
+
     this.#plugins[name] = session;
   }
 
   async dispatch(name: string, fn: string, args: unknown[]): Promise<unknown> {
     try {
-      const session = this.#plugins[name];
-      if (!session) {
+      const plugin = this.#plugins[name];
+      if (!plugin) {
         throw new Error(`No plugin '${name}' is registered`);
       }
-      return await session.call(fn, ...args);
+      return await plugin.call(fn, ...args);
     } catch (e) {
       // NOTE:
       // Vim/Neovim does not handle JavaScript Error instance thus use string instead
@@ -55,4 +73,36 @@ export class Service {
       });
     return Promise.resolve();
   }
+}
+
+function buildDispatcher(service: Service): DispatcherFrom<Api> {
+  const host = service.host;
+  const dispatcher: DispatcherFrom<Api> = {
+    async dispatch(
+      name: unknown,
+      fn: unknown,
+      args: unknown,
+    ): Promise<unknown> {
+      ensureString(name, 'name');
+      ensureString(fn, 'fn');
+      ensureArray(args, 'args');
+      return await service.dispatch(name, fn, args);
+    },
+    async call(fn: unknown, ...args: unknown[]): Promise<unknown> {
+      ensureString(fn, 'fn');
+      ensureArray(args, 'args');
+      return await host.call(fn, ...args);
+    },
+    async cmd(cmd: unknown, ctx: unknown): Promise<void> {
+      ensureString(cmd, 'cmd');
+      ensureRecord(ctx, 'ctx');
+      await host.cmd(cmd, ctx);
+    },
+    async eval(expr: unknown, ctx: unknown): Promise<unknown> {
+      ensureString(expr, 'expr');
+      ensureRecord(ctx, 'ctx');
+      return await host.eval(expr, ctx);
+    },
+  };
+  return dispatcher;
 }
