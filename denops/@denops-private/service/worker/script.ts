b83cf1443257cb224c907c2dbcbd6f1459e2c11b
Alisue <lambdalisue@hashnote.net>
2021-06-30 19:05:13 +0900

    :bug: Fix 'Unsupported scheme "c" for module' error on Windows
    Thanks to @takkii
    https://twitter.com/takkii/status/1410127506057744385?s=21

--- denops/@denops-private/service/worker/script.ts
+++ denops/@denops-private/service/worker/script.ts
@@ -1,6 +1,7 @@
 import {
   ensureObject,
   ensureString,
+  path,
   Session,
   using,
   WorkerReader,
@@ -14,7 +15,7 @@ const worker = self as any as Worker;
 async function main(name: string, script: string): Promise<void> {
   const reader = new WorkerReader(worker);
   const writer = new WorkerWriter(worker);
-  const mod = await import(script);
+  const mod = await import(path.toFileUrl(script).href);
   await using(
     new Session(reader, writer, {}, {
       errorCallback(e) {
