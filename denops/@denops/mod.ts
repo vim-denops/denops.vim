e97ca0e4f6921c512cb33107758bc6c3eb6c9452
Alisue <lambdalisue@hashnote.net>
2021-07-04 18:40:31 +0900

    :+1: Expose missing public types

--- denops/@denops/mod.ts
+++ denops/@denops/mod.ts
@@ -1,3 +1,4 @@
+export { BatchError } from "./denops.ts";
 // Re-export types
-export type { Context, Denops } from "./denops.ts";
+export type { Context, Denops, Meta } from "./denops.ts";
 export type { Dispatcher } from "./vendor/https/deno.land/x/msgpack_rpc/mod.ts";
