fb5107ffadae5a3c93f68c51fc1b3e415f978c2c
Alisue <lambdalisue@hashnote.net>
2021-10-30 23:03:20 +0900

    :+1: Define original `Dispatcher`

--- denops/@denops/mod.ts
+++ denops/@denops/mod.ts
@@ -1,4 +1,2 @@
 export { BatchError } from "./denops.ts";
-// Re-export types
-export type { Context, Denops, Meta } from "./denops.ts";
-export type { Dispatcher } from "https://deno.land/x/msgpack_rpc@v3.1.4/mod.ts#^";
+export type { Context, Denops, Dispatcher, Meta } from "./denops.ts";
