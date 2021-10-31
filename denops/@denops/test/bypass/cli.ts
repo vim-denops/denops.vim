a0c42264f99c23196a607c1819074a3fe1db1a8a
Alisue <lambdalisue@hashnote.net>
2021-10-31 15:35:13 +0900

    :bug: Revert bypass/cli.ts to fix `dispatch` test errors

--- denops/@denops/test/bypass/cli.ts
+++ denops/@denops/test/bypass/cli.ts
@@ -1,46 +1,17 @@
-import { Session } from "https://deno.land/x/msgpack_rpc@v3.1.4/mod.ts#^";
-import { BatchError, Denops } from "../../mod.ts";
+import { copy } from "https://deno.land/std@0.113.0/streams/conversion.ts";
+import {
+  WorkerReader,
+  WorkerWriter,
+} from "https://deno.land/x/workerio@v1.4.3/mod.ts#^";
 
-function unimplemented(): never {
-  throw new Error("unimplemented");
-}
+const worker = self as unknown as Worker;
+const reader = new WorkerReader(worker);
+const writer = new WorkerWriter(worker);
 
-export async function main(denops: Denops): Promise<void> {
-  denops.dispatcher = {
-    hello(name: unknown): Promise<unknown> {
-      return Promise.resolve(`Hello ${name}`);
-    },
-  };
-  const addr = JSON.parse(Deno.env.get("DENOPS_TEST_ADDRESS") || "");
-  const conn = await Deno.connect(addr);
-  // Build a service session defined in `@denops-private/service.ts` MANUALLY here
-  // because we should not rely on private API from public API.
-  const session = new Session(conn, conn, {
-    async call(fn: unknown, ...args: unknown[]): Promise<unknown> {
-      // deno-lint-ignore no-explicit-any
-      return await denops.call(fn as any, ...args);
-    },
+const addr = JSON.parse(Deno.env.get("DENOPS_TEST_ADDRESS") || "");
+const conn = await Deno.connect(addr);
 
-    async batch(...calls: unknown[]): Promise<unknown> {
-      try {
-        // deno-lint-ignore no-explicit-any
-        return [await denops.batch(...calls as any), ""];
-      } catch (e) {
-        if (e instanceof BatchError) {
-          return [e.results, e.message];
-        }
-        return [[], `${e}`];
-      }
-    },
-
-    async dispatch(
-      name: unknown,
-      fn: unknown,
-      args: unknown,
-    ): Promise<unknown> {
-      // deno-lint-ignore no-explicit-any
-      return await denops.dispatch(name as any, fn as any, args as any);
-    },
-  });
-  await session.waitClosed();
-}
+await Promise.race([
+  copy(conn, writer).finally(() => conn.close()),
+  copy(reader, conn),
+]);
