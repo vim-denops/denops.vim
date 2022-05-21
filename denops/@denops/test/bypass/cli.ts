385a7e271903a839606913e8162a99694904f258
Alisue <lambdalisue@gmail.com>
2022-05-21 15:11:51 +0900

    Update dependencies

--- denops/@denops/test/bypass/cli.ts
+++ denops/@denops/test/bypass/cli.ts
@@ -1,8 +1,8 @@
-import { copy } from "https://deno.land/std@0.127.0/streams/conversion.ts";
+import { copy } from "https://deno.land/std@0.140.0/streams/conversion.ts";
 import {
   WorkerReader,
   WorkerWriter,
-} from "https://deno.land/x/workerio@v1.4.3/mod.ts#^";
+} from "https://deno.land/x/workerio@v1.4.4/mod.ts#^";
 
 const worker = self as unknown as Worker;
 const reader = new WorkerReader(worker);
