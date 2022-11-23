74ff0c3bf670c6c9d1251c558a981c3ccde1a1f0
Alisue <lambdalisue@gmail.com>
2022-11-23 14:43:47 +0900

    :herb: Fix type-check on Deno 1.17

--- denops/@denops-private/worker/script.ts
+++ denops/@denops-private/worker/script.ts
@@ -34,8 +34,14 @@ async function main(name: string, script: string, meta: Meta): Promise<void> {
       // Protect the process itself from "Unhandled promises"
       // https://github.com/vim-denops/denops.vim/issues/208
       globalThis.addEventListener("unhandledrejection", (ev) => {
+        // XXX:
+        // Denops support Deno from 1.17 so the following `any` is required
+        // to pass type-check. Note that the code is not invoked because "unhandledrejection"
+        // event itself is supported from Deno 1.24 (and Deno 1.24 has `reason` attribute on `ev`)
+        // deno-lint-ignore no-explicit-any
+        const reason = (ev as any).reason;
         console.error(
-          `Unhandled rejection is detected. Worker of '${name}' will be reloaded: ${ev.reason}`,
+          `Unhandled rejection is detected. Worker of '${name}' will be reloaded: ${reason}`,
         );
         // Reload the worker because "Unhandled promises" error occured.
         session.notify("reload");
