e9469b0a5f656bb93aa8ced85d55469a1394b28e
Alisue <lambdalisue@gmail.com>
2023-01-07 13:15:43 +0900

    :shower: Do NOT use CSS directive
    The output is redirected to echomsg so highlight doesn't work anyway.

--- denops/@denops/test/mod.ts
+++ denops/@denops/test/mod.ts
@@ -5,8 +5,7 @@
  * @module
  */
 console.warn(
-  "%c[WARN] denops_core: This module is deprecated. Use https://deno.land/x/denops_test/mod.ts instead.",
-  "color: yellow;",
+  "denops_core: This module is deprecated. Use https://deno.land/x/denops_test/mod.ts instead.",
 );
 
 export { test } from "./tester.ts";
