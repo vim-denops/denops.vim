5076ef0cf9242dc99ef895794175a654ad60484f
Alisue <lambdalisue@gmail.com>
2023-07-19 14:15:03 +0900

    :coffee: Improve trace mode

--- denops/@denops-private/worker/trace_stream.ts
+++ denops/@denops-private/worker/trace_stream.ts
@@ -1,5 +1,9 @@
 import { pipeThroughFrom } from "https://deno.land/x/streamtools@v0.5.0/mod.ts";
 import { decode } from "https://deno.land/x/messagepack@v1.0.0/mod.ts";
+import {
+  isMessage,
+  Message,
+} from "https://deno.land/x/messagepack_rpc@v2.0.3/mod.ts";
 
 export function traceReadableStream(
   stream: ReadableStream<Uint8Array>,
@@ -7,7 +11,12 @@ export function traceReadableStream(
   return stream.pipeThrough(
     new TransformStream({
       transform(chunk, controller) {
-        console.log("RECV", decode(chunk));
+        const data = decode(chunk);
+        if (isMessage(data)) {
+          const m = stringifyMessage(data);
+          // Vim -> Denops
+          console.log("v -> d:", m);
+        }
         controller.enqueue(chunk);
       },
     }),
@@ -21,9 +30,37 @@ export function traceWritableStream(
     stream,
     new TransformStream({
       transform(chunk, controller) {
-        console.log("SENT", decode(chunk));
+        const data = decode(chunk);
+        if (isMessage(data)) {
+          const m = stringifyMessage(data);
+          // Denops -> Vim
+          console.log("d -> v:", m);
+        }
         controller.enqueue(chunk);
       },
     }),
   );
+}
+
+function stringifyMessage(m: Message): string {
+  switch (m[0]) {
+    case 0: {
+      const [_, msgid, method, params] = m;
+      const paramsStr = params.map((p) => JSON.stringify(p)).join(", ");
+      return `[${msgid}] request: ${method}(${paramsStr})`;
+    }
+    case 1: {
+      const [_, msgid, error, result] = m;
+      if (error) {
+        return `[${msgid}] response: ERROR: ${error}`;
+      } else {
+        return `[${msgid}] response: ${result}`;
+      }
+    }
+    case 2: {
+      const [_, method, params] = m;
+      const paramsStr = params.map((p) => JSON.stringify(p)).join(", ");
+      return `notify: ${method}(${paramsStr})`;
+    }
+  }
 }
