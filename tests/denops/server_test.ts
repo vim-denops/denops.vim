d14f1e91372b4d3fcc59160391d9dcdf33e1cac4
Milly <milly.ca@gmail.com>
2024-06-18 18:00:40 +0900

    :herb: fix flaky tests

--- tests/denops/server_test.ts
+++ tests/denops/server_test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from "jsr:@std/assert@0.225.2";
+import { assertEquals, assertFalse } from "jsr:@std/assert@0.225.2";
 import { testHost } from "../../denops/@denops-private/testutil/host.ts";
 import { wait } from "../../denops/@denops-private/testutil/wait.ts";
 
@@ -6,11 +6,6 @@ testHost({
   name: "denops#server#status()",
   mode: "all",
   fn: async (host, t) => {
-    await host.call("execute", [
-      "autocmd User DenopsReady let g:denops_ready_fired = 1",
-      "autocmd User DenopsClosed let g:denops_closed_fired = 1",
-    ], "");
-
     await t.step(
       "returns 'stopped' when no server running",
       async () => {
@@ -19,15 +14,30 @@ testHost({
       },
     );
 
+    // NOTE: The `plugin/denops.vim` must be sourced to initialize the environment.
+    await host.call("execute", [
+      "source plugin/denops.vim",
+      "autocmd User DenopsReady let g:denops_ready_fired = 1",
+      "autocmd User DenopsClosed let g:denops_closed_fired = 1",
+    ], "");
+
+    // Wait to stopped the server that starts by `plugin/denops.vim`
+    await wait(() => host.call("exists", "g:denops_ready_fired"));
+    await host.call("denops#server#stop");
+    await wait(() => host.call("eval", "denops#server#status() ==# 'stopped'"));
+
+    // NOTE: The status may transition to `preparing`, so get it within execute.
+    // SEE: https://github.com/vim-denops/denops.vim/issues/354
+    await host.call("execute", [
+      "unlet g:denops_ready_fired",
+      "unlet g:denops_closed_fired",
+      "call denops#server#start()",
+      "let g:__test_denops_server_status_when_start_called = denops#server#status()",
+    ], "");
+
     await t.step(
       "returns 'starting' when denops#server#start() is called",
       async () => {
-        // NOTE: The status may transition to `preparing`, so get it within execute.
-        // SEE: https://github.com/vim-denops/denops.vim/issues/354
-        await host.call("execute", [
-          "source plugin/denops.vim",
-          "let g:__test_denops_server_status_when_start_called = denops#server#status()",
-        ]);
         const actual = await host.call(
           "eval",
           "g:__test_denops_server_status_when_start_called",
@@ -37,12 +47,12 @@ testHost({
     );
 
     await t.step(
-      "returns 'preparing' before DenopsReady is fired",
+      "returns 'preparing' before DenopsReady is fired (flaky)",
       async () => {
         const actual = await wait(() =>
           host.call(
             "eval",
-            "exists('g:denops_ready_fired') ? 'DenopsReady is fired'" +
+            "exists('g:denops_ready_fired') ? 'DenopsReady is fired (flaky result)'" +
               ": denops#server#status() !=# 'starting' ? denops#server#status() : 0",
           )
         );
@@ -104,10 +114,20 @@ testHost({
       },
     );
 
+    await host.call("execute", [
+      "call denops#server#close()",
+      "let g:__test_denops_server_denops_closed_exists_after_close = exists('g:denops_closed_fired')",
+    ], "");
+
     await t.step(
-      "denops#server#close() closes the connection then DenopsClosed is fired",
+      "denops#server#close() closes the connection asynchronously then DenopsClosed is fired",
       async () => {
-        await host.call("denops#server#close");
+        assertFalse(
+          await host.call(
+            "eval",
+            "g:__test_denops_server_denops_closed_exists_after_close",
+          ),
+        );
         await wait(() => host.call("exists", "g:denops_closed_fired"));
       },
     );
