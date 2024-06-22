80eb0bfe76d5afd7369ee1c4658a4dd935669178
Milly <milly.ca@gmail.com>
2024-06-22 19:29:12 +0900

    :herb: improve `useSharedServer` helper

--- denops/@denops-private/testutil/shared_server_test.ts
+++ denops/@denops-private/testutil/shared_server_test.ts
@@ -1,37 +1,80 @@
 import {
   assertInstanceOf,
   assertMatch,
+  assertNotMatch,
   assertRejects,
 } from "jsr:@std/assert@0.225.2";
 import { delay } from "jsr:@std/async@0.224.0/delay";
-import { stub } from "jsr:@std/testing@0.224.0/mock";
+import { join } from "jsr:@std/path@0.225.0/join";
 import { useSharedServer } from "./shared_server.ts";
 
 Deno.test("useSharedServer()", async (t) => {
-  await t.step("returns `result.addr`", async () => {
-    await using server = await useSharedServer();
-    assertMatch(server.addr, /^127\.0\.0\.1:\d+$/);
+  await t.step("if `verbose` is not specified", async (t) => {
+    await t.step("returns `result.addr`", async () => {
+      await using server = await useSharedServer();
+      assertMatch(server.addr, /^127\.0\.0\.1:\d+$/);
+    });
+
+    await t.step("returns `result.stdout`", async () => {
+      await using server = await useSharedServer();
+      assertInstanceOf(server.stdout, ReadableStream);
+      const outputs: string[] = [];
+      server.stdout.pipeTo(
+        new WritableStream({ write: (line) => void outputs.push(line) }),
+      ).catch(() => {});
+      await delay(100);
+      assertMatch(outputs.join("\n"), /Listen denops clients on/);
+    });
+
+    await t.step("does not output stdout", async () => {
+      const proc = new Deno.Command(Deno.execPath(), {
+        args: [
+          "run",
+          "--allow-env",
+          "--allow-read",
+          "--allow-run",
+          join(import.meta.dirname!, "shared_server_test_no_verbose.ts"),
+        ],
+        stdout: "piped",
+      }).spawn();
+      const output = await proc.output();
+      const stdout = new TextDecoder().decode(output.stdout);
+      assertNotMatch(stdout, /Listen denops clients on/);
+    });
   });
 
-  await t.step("returns `result.stdout`", async () => {
-    await using server = await useSharedServer();
-    assertInstanceOf(server.stdout, ReadableStream);
-    const outputs: string[] = [];
-    server.stdout.pipeTo(
-      new WritableStream({
-        write: (line) => void outputs.push(line),
-      }),
-    ).catch(() => {});
-    await delay(100);
-    assertMatch(outputs.join("\n"), /Listen denops clients on/);
-  });
+  await t.step("if `verbose` is true", async (t) => {
+    await t.step("returns `result.addr`", async () => {
+      await using server = await useSharedServer({ verbose: true });
+      assertMatch(server.addr, /^127\.0\.0\.1:\d+$/);
+    });
 
-  await t.step("calls console.log() if `verbose` is true", async () => {
-    using console_log = stub(console, "log");
-    await using _server = await useSharedServer({ verbose: true });
-    await delay(100);
-    const outputs = console_log.calls.flatMap((call) => call.args).join("\n");
-    assertMatch(outputs, /Listen denops clients on/);
+    await t.step("returns `result.stdout`", async () => {
+      await using server = await useSharedServer({ verbose: true });
+      assertInstanceOf(server.stdout, ReadableStream);
+      const outputs: string[] = [];
+      server.stdout.pipeTo(
+        new WritableStream({ write: (line) => void outputs.push(line) }),
+      ).catch(() => {});
+      await delay(100);
+      assertMatch(outputs.join("\n"), /Listen denops clients on/);
+    });
+
+    await t.step("outputs stdout", async () => {
+      const proc = new Deno.Command(Deno.execPath(), {
+        args: [
+          "run",
+          "--allow-env",
+          "--allow-read",
+          "--allow-run",
+          join(import.meta.dirname!, "shared_server_test_verbose_true.ts"),
+        ],
+        stdout: "piped",
+      }).spawn();
+      const output = await proc.output();
+      const stdout = new TextDecoder().decode(output.stdout);
+      assertMatch(stdout, /Listen denops clients on/);
+    });
   });
 
   await t.step("closes child process when rejectes", async () => {
