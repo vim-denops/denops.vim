b83586b98b596d824988bf33ca3de988d9f16d3c
Alisue <lambdalisue@gmail.com>
2024-07-07 16:11:47 +0900

    :memo: Use JSR instead of deno.land

--- tests/denops/testdata/dummy_plugins/denops/dummy_valid/main.ts
+++ tests/denops/testdata/dummy_plugins/denops/dummy_valid/main.ts
@@ -1,4 +1,4 @@
-import type { Entrypoint } from "https://deno.land/x/denops_core@v6.1.0/mod.ts";
+import type { Entrypoint } from "jsr:@denops/core";
 
 export const main: Entrypoint = async (denops) => {
   await denops.cmd("echo 'Hello, Denops!'");
