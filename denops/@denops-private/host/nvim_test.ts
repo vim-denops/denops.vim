56e71b58957055dc65a1ac36511a794163ab0ed7
Milly <milly.ca@gmail.com>
2024-07-08 09:29:41 +0900

    :herb: improve `Vim` and `Neovim` tests

--- denops/@denops-private/host/nvim_test.ts
+++ denops/@denops-private/host/nvim_test.ts
@@ -5,19 +5,22 @@ import {
   assertStringIncludes,
 } from "jsr:@std/assert@0.225.1";
 import {
-  assertSpyCall,
+  assertSpyCallArgs,
   assertSpyCalls,
+  resolvesNext,
   stub,
 } from "jsr:@std/testing@0.224.0/mock";
 import { delay } from "jsr:@std/async@0.224.0/delay";
 import { promiseState } from "jsr:@lambdalisue/async@2.1.1";
 import { unimplemented } from "jsr:@lambdalisue/errorutil@1.0.0";
+import { Client } from "jsr:@lambdalisue/messagepack-rpc@2.4.0";
 import { withNeovim } from "/denops-testutil/with.ts";
 import type { Service } from "../host.ts";
 import { Neovim } from "./nvim.ts";
 
+const NOTIFY_DELAY = 100;
+
 Deno.test("Neovim", async (t) => {
-  let waitClosed: Promise<void> | undefined;
   await withNeovim({
     fn: async ({ reader, writer }) => {
       const service: Service = {
@@ -32,142 +35,381 @@ Deno.test("Neovim", async (t) => {
       };
 
       await using host = new Neovim(reader, writer);
-      await t.step(
-        "'invoke' message before init throws error",
-        async () => {
-          await assertRejects(
-            () =>
-              host.call(
-                "denops#_internal#test#request",
-                "invoke",
-                ["reload", ["dummy"]],
-              ),
-            Error,
-            "Failed to call",
-          );
-        },
-      );
 
-      await t.step("init() calls Service.bind()", async () => {
-        using s = stub(service, "bind");
-        await host.init(service);
-        assertSpyCall(s, 0, { args: [host] });
+      await t.step("before .init() calls", async (t) => {
+        await t.step("when handle message", async (t) => {
+          await t.step("'invoke' rejects", async () => {
+            await assertRejects(
+              () =>
+                host.call(
+                  "denops#_internal#test#request",
+                  "invoke",
+                  ["reload", ["dummy"]],
+                ),
+              Error,
+              "No service is registered in the host",
+            );
+          });
+        });
       });
 
-      await t.step("redraw() does nothing", async () => {
-        await host.redraw();
+      await t.step(".init()", async (t) => {
+        await t.step("calls service.bind()", async () => {
+          using service_bind = stub(service, "bind");
+
+          await host.init(service);
+
+          assertSpyCallArgs(service_bind, 0, [host]);
+        });
       });
 
-      await t.step("call() returns a result of the function", async () => {
-        const result = await host.call("abs", -4);
-        assertEquals(result, 4);
+      await t.step(".redraw()", async (t) => {
+        await t.step("does nothing", async () => {
+          await host.redraw();
+        });
       });
 
-      await t.step(
-        "call() throws an error when failed to call the function",
-        async () => {
-          await assertRejects(
-            () => host.call("@@@@@", -4),
-            Error,
-            "Failed to call '@@@@@' in Neovim: Vim:E117: Unknown function: @@@@@ (code: 0)",
-          );
-        },
-      );
+      await t.step(".call()", async (t) => {
+        await t.step("resolves a result of `fn`", async () => {
+          const result = await host.call("abs", -4);
 
-      await t.step("batch() returns results of the functions", async () => {
-        const [ret, err] = await host.batch(
-          ["abs", -4],
-          ["abs", 10],
-          ["abs", -9],
-        );
-        assertEquals(ret, [4, 10, 9]);
-        assertEquals(err, "");
+          assertEquals(result, 4);
+        });
+
+        await t.step("if `fn` does not exist", async (t) => {
+          await t.step("rejects with an error", async () => {
+            await assertRejects(
+              () => host.call("@@@@@", -4),
+              Error,
+              "Failed to call '@@@@@' in Neovim: Vim:E117: Unknown function: @@@@@ (code: 0)",
+            );
+          });
+        });
+
+        await t.step("if Client error occurs", async (t) => {
+          await t.step("rejects with an error", async () => {
+            using _client_call = stub(
+              Client.prototype,
+              "call",
+              resolvesNext([new Error("Client call error")]),
+            );
+
+            await assertRejects(
+              () => host.call("abs", -4),
+              Error,
+              "Client call error",
+            );
+          });
+        });
       });
 
-      await t.step(
-        "batch() returns resutls with an error when failed to call the function",
-        async () => {
+      await t.step(".batch()", async (t) => {
+        await t.step("resolves results of `calls`", async () => {
           const [ret, err] = await host.batch(
             ["abs", -4],
             ["abs", 10],
-            ["@@@@@", -9],
+            ["abs", -9],
           );
-          assertEquals(ret, [4, 10]);
-          assertMatch(
-            err,
-            /Failed to call '@@@@@' in Neovim: Vim:E117: Unknown function: @@@@@/,
-          );
-        },
-      );
 
-      await t.step("notify() calls the function", () => {
-        host.notify("abs", -4);
+          assertEquals(ret, [4, 10, 9]);
+          assertEquals(err, "");
+        });
+
+        await t.step("if some function does not exist", async (t) => {
+          await t.step("resolves resutls and an error", async () => {
+            const [ret, err] = await host.batch(
+              ["abs", -4],
+              ["abs", 10],
+              ["@@@@@", -9],
+              ["abs", 10],
+            );
+
+            assertEquals(ret, [4, 10]);
+            assertMatch(
+              err,
+              /^Failed to call '@@@@@' in Neovim: Vim:E117: Unknown function: @@@@@/,
+            );
+          });
+
+          await t.step("does not call functions after failure", async () => {
+            await host.call("execute", [
+              "let g:__test_host_batch_fn_calls = []",
+              "function! TestHostBatchFn(...) abort",
+              "  call add(g:__test_host_batch_fn_calls, a:000)",
+              "endfunction",
+            ], "");
+
+            await host.batch(
+              ["TestHostBatchFn", -4],
+              ["TestHostBatchFn", 10],
+              ["@@@@@", 10],
+              ["TestHostBatchFn", -9],
+              ["TestHostBatchFn", -4],
+            );
+
+            const actual = await host.call(
+              "eval",
+              "g:__test_host_batch_fn_calls",
+            );
+            assertEquals(actual, [[-4], [10]]);
+          });
+        });
+
+        await t.step("if Client error occurs", async (t) => {
+          await t.step("rejects with an error", async () => {
+            using _client_call = stub(
+              Client.prototype,
+              "call",
+              resolvesNext([new Error("Client call error")]),
+            );
+
+            await assertRejects(
+              () => host.batch(["abs", -4]),
+              Error,
+              "Client call error",
+            );
+          });
+        });
       });
 
-      await t.step(
-        "notify() does not throws if calls a non-existent function",
-        async () => {
+      await t.step(".notify()", async (t) => {
+        await t.step("calls `fn`", async () => {
+          await host.call("execute", [
+            "let g:__test_host_notify_fn_calls = []",
+            "function! TestHostNotifyFn(...) abort",
+            "  call add(g:__test_host_notify_fn_calls, a:000)",
+            "endfunction",
+          ], "");
+
+          await host.notify(
+            "TestHostNotifyFn",
+            "foo",
+            4,
+            undefined,
+            null,
+            false,
+          );
+
+          await delay(NOTIFY_DELAY); // maybe flaky
+          const actual = await host.call(
+            "eval",
+            "g:__test_host_notify_fn_calls",
+          );
+          assertEquals(actual, [["foo", 4, 0, null, false]]);
+        });
+
+        await t.step("if `fn` does not exist", async (t) => {
           using console_error = stub(console, "error");
-          host.notify("@@@@@", -4); // should not throw
-          await delay(100); // maybe flaky
-          assertSpyCalls(console_error, 1);
-          assertStringIncludes(
-            console_error.calls.flatMap((c) => c.args).join(" "),
-            "nvim_error_event(0) Vim:E117: Unknown function: @@@@@",
-          );
-        },
-      );
 
-      await t.step(
-        "'void' message does nothing",
-        async () => {
-          await host.call(
-            "denops#_internal#test#request",
-            "void",
-            [],
-          );
-        },
-      );
+          await t.step("does not reject", async () => {
+            await host.notify("@@@@@", -4);
+          });
 
-      await t.step(
-        "'invoke' message calls Service method",
-        async () => {
-          using s = stub(service, "reload");
-          await host.call(
-            "denops#_internal#test#request",
-            "invoke",
-            ["reload", ["dummy"]],
-          );
-          assertSpyCall(s, 0, { args: ["dummy"] });
-        },
-      );
+          await t.step("outputs an error message", async () => {
+            await delay(NOTIFY_DELAY); // maybe flaky
+            assertSpyCalls(console_error, 1);
+            assertStringIncludes(
+              console_error.calls.flatMap((c) => c.args).join(" "),
+              "nvim_error_event(0) Vim:E117: Unknown function: @@@@@",
+            );
+          });
+        });
+      });
 
-      await t.step(
-        "'nvim_error_event' message shows error message",
-        async () => {
-          using s = stub(console, "error");
-          await host.call(
-            "denops#_internal#test#request",
-            "nvim_error_event",
-            [0, "message"],
-          );
-          assertSpyCall(s, 0, { args: ["nvim_error_event(0)", "message"] });
-        },
-      );
+      await t.step("when handle request message", async (t) => {
+        await t.step("'void'", async (t) => {
+          await t.step("does nothing", async () => {
+            await host.call(
+              "denops#_internal#test#request",
+              "void",
+              [],
+            );
+          });
+        });
 
-      await t.step(
-        "waitClosed() returns a promise that is pending when the session is not closed",
-        async () => {
-          waitClosed = host.waitClosed();
-          assertEquals(await promiseState(waitClosed), "pending");
-        },
-      );
+        await t.step("'invoke'", async (t) => {
+          await t.step("calls Service method", async () => {
+            using service_reload = stub(service, "reload");
+
+            await host.call(
+              "denops#_internal#test#request",
+              "invoke",
+              ["reload", ["dummy"]],
+            );
+
+            assertSpyCallArgs(service_reload, 0, ["dummy"]);
+          });
+
+          await t.step("resolves a result of Service method", async () => {
+            using _service_dispatch = stub(
+              service,
+              "dispatch",
+              resolvesNext([{ foo: "dummy result" }]),
+            );
+
+            const actual = await host.call(
+              "denops#_internal#test#request",
+              "invoke",
+              ["dispatch", ["dummy", "fn", ["arg0"]]],
+            );
+
+            assertEquals(actual, { foo: "dummy result" });
+          });
+
+          await t.step("if Service method rejects", async (t) => {
+            await t.step("rejects with an error", async () => {
+              using _service_dispatch = stub(
+                service,
+                "dispatch",
+                () => Promise.reject("Error: stringified error message"),
+              );
+
+              await assertRejects(
+                () =>
+                  host.call(
+                    "denops#_internal#test#request",
+                    "invoke",
+                    ["dispatch", ["dummy", "fn", ["arg0"]]],
+                  ),
+                Error,
+                "Error: stringified error message",
+              );
+            });
+          });
+        });
+
+        await t.step("'nvim_error_event'", async (t) => {
+          await t.step("outputs an error message", async () => {
+            using console_error = stub(console, "error");
+
+            await host.call(
+              "denops#_internal#test#request",
+              "nvim_error_event",
+              [0, "message"],
+            );
+
+            assertSpyCallArgs(console_error, 0, [
+              "nvim_error_event(0)",
+              "message",
+            ]);
+          });
+        });
+
+        await t.step("unknown message", async (t) => {
+          await t.step("rejects with an error", async () => {
+            await assertRejects(
+              () =>
+                host.call(
+                  "denops#_internal#test#request",
+                  "unknown_message",
+                  [0, "message"],
+                ),
+              Error,
+              "NoMethodFoundError: No MessagePack-RPC method 'unknown_message' exists",
+            );
+          });
+        });
+      });
+
+      await t.step("when handle notify message", async (t) => {
+        await t.step("'void'", async (t) => {
+          await t.step("does nothing", async () => {
+            await host.call(
+              "denops#_internal#test#notify",
+              "void",
+              [],
+            );
+          });
+        });
+
+        await t.step("'invoke'", async (t) => {
+          await t.step("calls Service method", async () => {
+            using service_reload = stub(service, "reload");
+
+            await host.call(
+              "denops#_internal#test#notify",
+              "invoke",
+              ["reload", ["dummy"]],
+            );
+
+            assertSpyCallArgs(service_reload, 0, ["dummy"]);
+          });
+
+          await t.step("if Service method rejects", async (t) => {
+            await t.step("outputs an error message", async () => {
+              using console_error = stub(console, "error");
+              using _service_dispatch = stub(
+                service,
+                "dispatch",
+                () => Promise.reject("Error: stringified error message"),
+              );
+
+              await host.call(
+                "denops#_internal#test#notify",
+                "invoke",
+                ["dispatch", ["dummy", "fn", ["arg0"]]],
+              );
+
+              assertSpyCalls(console_error, 1);
+              assertMatch(
+                console_error.calls[0].args.join(" "),
+                /^Failed to handle message [0-9]+,invoke,dispatch,dummy,fn,arg0/,
+              );
+            });
+          });
+        });
+
+        await t.step("'nvim_error_event'", async (t) => {
+          await t.step("outputs an error message", async () => {
+            using console_error = stub(console, "error");
+
+            await host.call(
+              "denops#_internal#test#notify",
+              "nvim_error_event",
+              [0, "message"],
+            );
+
+            assertSpyCallArgs(console_error, 0, [
+              "nvim_error_event(0)",
+              "message",
+            ]);
+          });
+        });
+
+        await t.step("unknown message", async (t) => {
+          await t.step("outputs an error message", async () => {
+            using console_error = stub(console, "error");
+
+            await host.call(
+              "denops#_internal#test#notify",
+              "unknown_message",
+              [0, "message"],
+            );
+
+            assertSpyCalls(console_error, 1);
+            assertMatch(
+              console_error.calls[0].args.join(" "),
+              /^Failed to handle message [0-9]+,unknown_message,0,message/,
+            );
+          });
+        });
+      });
+
+      // NOTE: This test closes the session of the host.
+      await t.step(".waitClosed()", async (t) => {
+        const waitClosedPromise = host.waitClosed();
+
+        await t.step("pendings before the session closes", async () => {
+          assertEquals(await promiseState(waitClosedPromise), "pending");
+        });
+
+        // NOTE: Close the session of the host.
+        await host[Symbol.asyncDispose]();
+
+        await t.step("fulfilled when the session closes", async () => {
+          assertEquals(await promiseState(waitClosedPromise), "fulfilled");
+        });
+      });
     },
   });
-  await t.step(
-    "waitClosed promise is fulfilled when the session is closed",
-    async () => {
-      assertEquals(await promiseState(waitClosed!), "fulfilled");
-    },
-  );
 });
