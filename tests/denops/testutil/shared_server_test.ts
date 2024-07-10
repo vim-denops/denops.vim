7c49bb84f7ca5d61bd3714cbe60f5f771a606f29
Milly <milly.ca@gmail.com>
2024-07-11 08:10:54 +0900

    :herb: ignore test if `DENOPS_TEST_VERBOSE` env exist

--- tests/denops/testutil/shared_server_test.ts
+++ tests/denops/testutil/shared_server_test.ts
@@ -10,48 +10,54 @@ import { join } from "jsr:@std/path@0.225.0/join";
 import { useSharedServer } from "./shared_server.ts";
 
 Deno.test("useSharedServer()", async (t) => {
-  await t.step("if `verbose` is not specified", async (t) => {
-    await t.step("returns `result.addr`", async (t) => {
-      await using server = await useSharedServer({ verbose: true });
-      const { addr } = server;
+  const HAS_DENOPS_TEST_VERBOSE = Deno.env.has("DENOPS_TEST_VERBOSE");
 
-      await t.step("`addr.host` is string", () => {
-        assertEquals(addr.host, "127.0.0.1");
-      });
-      await t.step("`addr.port` is number", () => {
-        assertEquals(typeof addr.port, "number");
-      });
-      await t.step("`addr.toString()` returns the address", () => {
-        assertMatch(addr.toString(), /^127\.0\.0\.1:\d+$/);
-      });
-    });
+  await t.step({
+    name: "if `verbose` is not specified",
+    ignore: HAS_DENOPS_TEST_VERBOSE,
+    fn: async (t) => {
+      await t.step("returns `result.addr`", async (t) => {
+        await using server = await useSharedServer();
+        const { addr } = server;
 
-    await t.step("returns `result.stdout`", async () => {
-      await using server = await useSharedServer();
-      assertInstanceOf(server.stdout, ReadableStream);
-      const outputs: string[] = [];
-      server.stdout.pipeTo(
-        new WritableStream({ write: (line) => void outputs.push(line) }),
-      ).catch(() => {});
-      await delay(100);
-      assertMatch(outputs.join("\n"), /Listen denops clients on/);
-    });
+        await t.step("`addr.host` is string", () => {
+          assertEquals(addr.host, "127.0.0.1");
+        });
+        await t.step("`addr.port` is number", () => {
+          assertEquals(typeof addr.port, "number");
+        });
+        await t.step("`addr.toString()` returns the address", () => {
+          assertMatch(addr.toString(), /^127\.0\.0\.1:\d+$/);
+        });
+      });
 
-    await t.step("does not output stdout", async () => {
-      const proc = new Deno.Command(Deno.execPath(), {
-        args: [
-          "run",
-          "--allow-env",
-          "--allow-read",
-          "--allow-run",
-          resolve("shared_server_test_no_verbose.ts"),
-        ],
-        stdout: "piped",
-      }).spawn();
-      const output = await proc.output();
-      const stdout = new TextDecoder().decode(output.stdout);
-      assertNotMatch(stdout, /Listen denops clients on/);
-    });
+      await t.step("returns `result.stdout`", async () => {
+        await using server = await useSharedServer();
+        assertInstanceOf(server.stdout, ReadableStream);
+        const outputs: string[] = [];
+        server.stdout.pipeTo(
+          new WritableStream({ write: (line) => void outputs.push(line) }),
+        ).catch(() => {});
+        await delay(100);
+        assertMatch(outputs.join("\n"), /Listen denops clients on/);
+      });
+
+      await t.step("does not output stdout", async () => {
+        const proc = new Deno.Command(Deno.execPath(), {
+          args: [
+            "run",
+            "--allow-env",
+            "--allow-read",
+            "--allow-run",
+            resolve("shared_server_test_no_verbose.ts"),
+          ],
+          stdout: "piped",
+        }).spawn();
+        const output = await proc.output();
+        const stdout = new TextDecoder().decode(output.stdout);
+        assertNotMatch(stdout, /Listen denops clients on/);
+      });
+    },
   });
 
   await t.step("if `verbose` is true", async (t) => {
