8dda96ab328c6634fa94a1c1a1d051d60890c714
Milly <milly.ca@gmail.com>
2024-09-14 01:56:48 +0900

    :herb: add test cases of invalid plugin name

--- tests/denops/runtime/functions/plugin/wait_async_test.ts
+++ tests/denops/runtime/functions/plugin/wait_async_test.ts
@@ -5,6 +5,7 @@ import {
   assertRejects,
 } from "jsr:@std/assert@^1.0.1";
 import { delay } from "jsr:@std/async@^1.0.1";
+import { INVALID_PLUGIN_NAMES } from "/denops-testdata/invalid_plugin_names.ts";
 import { resolveTestDataPath } from "/denops-testdata/resolve.ts";
 import { testHost } from "/denops-testutil/host.ts";
 import { wait } from "/denops-testutil/wait.ts";
@@ -29,19 +30,20 @@ testHost({
       "autocmd User DenopsPlugin* call add(g:__test_denops_events, expand('<amatch>'))",
     ], "");
 
-    await t.step("if the plugin name is invalid", async (t) => {
-      await t.step("throws an error", async () => {
-        // NOTE: '.' is not allowed in plugin name.
-        await assertRejects(
-          () =>
-            host.call("execute", [
-              "call denops#plugin#wait_async('dummy.invalid', { -> 0 })",
-            ], ""),
-          Error,
-          "Invalid plugin name: dummy.invalid",
-        );
+    for (const [plugin_name, label] of INVALID_PLUGIN_NAMES) {
+      await t.step(`if the plugin name is invalid (${label})`, async (t) => {
+        await t.step("throws an error", async () => {
+          await assertRejects(
+            () =>
+              host.call("execute", [
+                `call denops#plugin#wait_async('${plugin_name}', { -> 0 })`,
+              ], ""),
+            Error,
+            `Invalid plugin name: ${plugin_name}`,
+          );
+        });
       });
-    });
+    }
 
     await t.step("if the plugin is load asynchronously", async (t) => {
       // Load plugin asynchronously.
