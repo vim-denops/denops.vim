5b0084c6ca8b67f18ac8b312dd8662227d9febda
Milly <milly.ca@gmail.com>
2025-06-18 21:30:57 +0900

    :herb: Preserve dependencies in test fixture

--- tests/denops/testdata/no_check/cli_constraint_error_on_issue_401.ts
+++ tests/denops/testdata/no_check/cli_constraint_error_on_issue_401.ts
@@ -1,1 +1,9 @@
-import * as _ from "jsr:@std/async@1.0.0-constraint-error";
+// This is a test code that triggers a constraint error.
+// It specifies a non-existent version of an existing package.
+//
+// The `@std/internal` package exists, but it is not imported from the project.
+// It is used to exclude updates with `molt --ignore`.
+// See the tasks in *deno.jsonc* for details.
+//
+// DO NOT UPDATE THIS LINE.
+import * as _ from "jsr:@std/internal@1.0.0-non-existent-version";
