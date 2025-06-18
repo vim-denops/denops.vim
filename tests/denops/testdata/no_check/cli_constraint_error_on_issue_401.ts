92852b9af1c396a2de061903bf4de9061598bd2e
Milly <milly.ca@gmail.com>
2025-06-18 21:30:57 +0900

    :herb: Add comments to clarify test purpose

--- tests/denops/testdata/no_check/cli_constraint_error_on_issue_401.ts
+++ tests/denops/testdata/no_check/cli_constraint_error_on_issue_401.ts
@@ -1,1 +1,4 @@
-import * as _ from "jsr:@std/async@1.0.13";
+// This is a test code that triggers a constraint error.
+// It specifies a non-existent version of an existing package.
+// DO NOT UPDATE THIS LINE.
+import * as _ from "jsr:@std/async@1.0.0-constraint-error";
